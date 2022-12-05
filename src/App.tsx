e038e10a19d55e324a31d468befd61f254639d4c
omer <oemer.peynirci@ontavio.de>
2022-12-05 21:24:09 +0100

    up to 800px width style completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-
-import './App.css'
+import styles from './App.module.css';
+import logo from './logo.svg'
+import {FaBars} from 'react-icons/fa';
 
 function App() {
 
   return (
     <div className="App">
+<nav className={styles.nav}>
+    <div className={styles.navHeader}>
+        <img src={logo} alt={"logo"} className={styles.logo}/>
+        <button className={styles.btnMenu}>
+
+            <FaBars/>
+        </button>
+    </div>
+<div className={styles.linkContainer}>
+    <ul className={styles.linkList}>
+        <li className={styles.linkItem}><a href="#">Home</a></li>
+        <li className={styles.linkItem}><a href="#">About</a></li>
+        <li className={styles.linkItem}><a href="#">Projects</a></li>
+        <li className={styles.linkItem}><a href="#">Contact</a></li>
+        <li className={styles.linkItem}><a href="#">Profile</a></li>
+    </ul>
+</div>
+</nav>
 
     </div>
   )
